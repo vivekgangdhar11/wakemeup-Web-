0a9599c5a8a088d27d137c97d94f56b749822f13
PGZON <prathameshgadgil2005@gmail.com>
2025-03-22 15:45:53 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
         </p>
-     <h1>Hello Team</h1>
+     <h1>Hello ABC</h1>
       </header>
     </div>
   );
