efdbfeed756c2bf27ec50f957e46a59bd44d63de
PGZON <prathameshgadgil2005@gmail.com>
2025-03-22 16:11:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <h1>Hello ABC</h1>
-      <h3>vivek here</h3>
-      </header>
-    </div>
+  <>
+  </>
+    
   );
 }
 
